bcc53bdb3fa14856ea70b7356628b1eb3f3344cb
Varun Sathish <varunsathish15@gmail.com>
2026-06-22 16:52:45 +0530

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,13 @@
+﻿d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+user_input = input('Enter a key: ')
+
+try:
+    key = int(user_input)
+except ValueError:
+    print('Invalid input. Please enter an integer key.')
+else:
+    if key in d:
+        print('Value for key', key, 'is', d[key])
+    else:
+        print('Key not found in dictionary.')
